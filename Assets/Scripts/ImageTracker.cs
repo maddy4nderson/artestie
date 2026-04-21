15b17411d2db6ff5d6cb00ee532571d2fdf0a082
maddy <maddy4nderson@gmail.com>
2026-04-21 12:55:06 +0100

    window

--- Assets/Scripts/ImageTracker.cs
+++ Assets/Scripts/ImageTracker.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using System.Collections.Generic;
+using UnityEngine.XR.ARSubsystems;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class ImageTracker : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+ARTrackedImageManager m_ImageManager;
+public GameObject noosaScene;
+void OnEnable() => m_ImageManager.trackablesChanged.AddListener(OnChanged);
+
+void OnDisable() => m_ImageManager.trackablesChanged.RemoveListener(OnChanged);
+
+void OnChanged(ARTrackablesChangedEventArgs<ARTrackedImage> eventArgs)
+{
+    foreach (var newImage in eventArgs.added)
     {
-        
+        GameObject newObject = Instantiate(noosaScene);
+        newObject.transform.SetParent(newImage.transform, false);
+
+        // Handle added event
     }
 
-    // Update is called once per frame
-    void Update()
+    foreach (var updatedImage in eventArgs.updated)
     {
-        
+        // Handle updated event
     }
+
+    foreach (var removed in eventArgs.removed)
+    {
+        // Handle removed event
+        TrackableId removedImageTrackableId = removed.Key;
+        ARTrackedImage removedImage = removed.Value;
+    }
+}
+
 }
