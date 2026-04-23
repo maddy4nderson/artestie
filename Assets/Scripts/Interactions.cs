b55849529fc73cfac779ecf50ffb7a61e8f23990
maddy <maddy4nderson@gmail.com>
2026-04-23 18:15:33 +0100

    code

--- Assets/Scripts/Interactions.cs
+++ Assets/Scripts/Interactions.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class Interactions : MonoBehaviour
 {
-   public void selected()
-   {
-        Debug.Log("object selected");
-   }
+
+    bool annotationVisible = false; //current visibility of annotation
+    public GameObject annotation; // annotation object
+
+    public void selected()
+    {
+
+        Debug.Log("show annotation");
+        //toggle visibility of the annotation
+        if (annotationVisible)
+        {
+            annotation.SetActive(false);
+            annotationVisible = false;
+        }else {
+            annotation.SetActive(true);
+            annotationVisible = true;
+        }
+
+    }
 }
